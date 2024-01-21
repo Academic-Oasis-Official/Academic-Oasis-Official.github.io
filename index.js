f7cfc7879457be8f955b6f6687c794ce1adac8b0
Eric Beaulne <eric.beaulne@ontariotechu.net>
2024-01-21 13:56:53 -0500

    feat: complete UI overhaul

---
+++ index.js
@@ -0,0 +1,42 @@
+const COMPLETED = 'Completed';
+const IN_PROGRESS = 'In Progress';
+const PLANNED = 'Planned';
+
+const STATUS_CLASS = {
+    [COMPLETED]: 'done',
+    [IN_PROGRESS]: 'in-prog',
+    [PLANNED]: 'planned',
+}
+
+const map = [
+    {
+        title: "Website",
+        description: "Create a website for the company",
+        status: IN_PROGRESS,
+    },
+    {
+        title: "Discord Server",
+        description: "Create a discord server for the company",
+        status: COMPLETED,
+    }
+]
+
+function addItems() {
+    const container = document.getElementById('card-container');
+
+    map.forEach(item => {
+        const card = `
+        <div class="card">
+            <div class="title-row">
+                <h3 class="title">${item.title}</h2>
+                <div class="status ${STATUS_CLASS[item.status]}">${item.status}</div>
+            </div>
+            <p>${item.description}</p>
+        </div>
+        `
+
+        container.innerHTML += card;
+    })
+}
+
+addItems();
